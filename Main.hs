a7e807bdc0dbccc2f36a7a48531fbcb31bad1ec1
annathais <annathais17@gmail.com>
2019-11-18 00:16:39 -0300

    comments align

--- Main.hs
+++ Main.hs
@@ -21,53 +21,53 @@ altura = 20
 
 main :: IO ()
 main = do
-  putStrLn "\n--------------------------------------------------------------"
+  putStrLn "\n----------------------------am----------------------------------"
   putStrLn "--            --> Bem vindo ao Campo Minado <--             --"
   putStrLn "--------------------------------------------------------------\n"
-  g        <- getStdGen                                   -- Obtém o gerador global de números aleatórios.
-  campo    <- return $ fazMatriz largura altura Fechado   -- Chama a função fazMatriz e passa o parâmetro largura, altura e Fechado
-  minas    <- return $ gerarJogo largura altura (40) g    -- Chama a função gerarJogo e passa o parâmetro largura altura nº de bombas e os números aleatórios
-  jogar campo minas >>= mostrarMinas                      {- Chama a função jogar e passa o parâmetro campo e minas, >>= passa o resultado da expressão à esquerda 
-                                                             como argumento para a expressão à direita, de uma maneira que respeite o contexto do argumento e da função usada-}
+  g        <- getStdGen                                         -- Obtém o gerador global de números aleatórios.
+  campo    <- return $ fazMatriz largura altura Fechado         -- Chama a função fazMatriz e passa o parâmetro largura, altura e Fechado
+  minas    <- return $ gerarJogo largura altura (40) g          -- Chama a função gerarJogo e passa o parâmetro largura altura nº de bombas e os números aleatórios
+  jogar campo minas >>= mostrarMinas                            {- Chama a função jogar e passa o parâmetro campo e minas, >>= passa o resultado da expressão à esquerda 
+                                                                   como argumento para a expressão à direita, de uma maneira que respeite o contexto do argumento e da função usada-}
 
 
 jogar :: Campo -> Minas -> IO Campo
 jogar e w = do
-  mostrarMinas e                                            -- Chama a função mostrarMinas passado o parêmtro e (que é o campo mandado na função main)
+  mostrarMinas e                                                -- Chama a função mostrarMinas passado o parêmtro e (que é o campo mandado na função main)
   putStrLn "Escolha uma coordenada (x y):"
-  jogada <- getLine                                         -- Captura a jogada
-  novoC  <- return $ analisar w (analisaEntrada jogada) e   -- Converte a string de entrada "jogada" em um par de coordenada
-  putStrLn $ case bombaVisivel novoC of                     -- Caso tenha uma bomba visivel apos a jogadada,
-    True -> "\nVoce perdeu!"                                -- a mensagem diz que voce perde.
-    False -> "\nContinue"                                   -- Caso contrario, continua
-  case bombaVisivel novoC of                                -- Mesmo caso de cima, mas com a atualizacao do tabuleiro
-      True  -> return novoC                                 -- Caso perca, imprime o tabuleiro atualizado atual com a bomba visivel
-      False -> jogar novoC w                                -- Caso contrario, retorna para a funcao de jogar
+  jogada <- getLine                                             -- Captura a jogada
+  novoC  <- return $ analisar w (analisaEntrada jogada) e       -- Converte a string de entrada "jogada" em um par de coordenada
+  putStrLn $ case bombaVisivel novoC of                         -- Caso tenha uma bomba visivel apos a jogadada,
+    True -> "\nVoce perdeu!"                                    -- a mensagem diz que voce perde.
+    False -> "\nContinue"                                       -- Caso contrario, continua
+  case bombaVisivel novoC of                                    -- Mesmo caso de cima, mas com a atualizacao do tabuleiro
+      True  -> return novoC                                     -- Caso perca, imprime o tabuleiro atualizado atual com a bomba visivel
+      False -> jogar novoC w                                    -- Caso contrario, retorna para a funcao de jogar
 
 
-analisar :: Minas -> Coordenada -> Campo -> Campo           -- Analisa se a jogada foi em uma coordenada ja aberta
-analisar w p@(x, y) e = case e !! x !! y of                 -- Caso a coordenada ainda esteja fechada,
-                            Fechado -> atualizarCampo w p e -- chama a funcao para atualizar o campo, abrindo-a
-                            _       -> e                    -- caso ja esteja aberta, retorna o proprio campo
+analisar :: Minas -> Coordenada -> Campo -> Campo               -- Analisa se a jogada foi em uma coordenada ja aberta
+analisar w p@(x, y) e = case e !! x !! y of                     -- Caso a coordenada ainda esteja fechada,
+                            Fechado -> atualizarCampo w p e     -- chama a funcao para atualizar o campo, abrindo-a
+                            _       -> e                        -- caso ja esteja aberta, retorna o proprio campo
 
-atualizarCampo :: Minas -> Coordenada -> Campo -> Campo     -- Atualiza o campo impresso
+atualizarCampo :: Minas -> Coordenada -> Campo -> Campo         -- Atualiza o campo impresso
 atualizarCampo w p@(x, y) e =
     case estado of
       Peso 0 -> foldr (analisar w) novoCampo $ emTorno largura altura p   -- Caso seja uma coordenada de peso 0 (sem bombas adjacentes), analisa os adjacentes
-      _      -> novoCampo                                   -- ao achar algum que nao tenha peso 0, imprime novo campo
+      _      -> novoCampo                                                 -- ao achar algum que nao tenha peso 0, imprime novo campo
     where
       estado    = (w !! x !! y)
-      novoCampo = trocarIndiceMatriz p e estado             -- onde o novo campo possui um caracter referente ao peso != 0
+      novoCampo = trocarIndiceMatriz p e estado                           -- onde o novo campo possui um caracter referente ao peso != 0
 
 
-bombaVisivel :: Campo -> Bool                               -- verifica se tem bomba visivel apos atualizar o campo
-bombaVisivel [] = False                                     -- campo vazio = falso, nenhuma bomba viivel
+bombaVisivel :: Campo -> Bool                                   -- verifica se tem bomba visivel apos atualizar o campo
+bombaVisivel [] = False                                         -- campo vazio = falso, nenhuma bomba viivel
 bombaVisivel (xs:xss)
-              | temBomba xs     = True                      -- Se o estado do campo for Bomba, aparece uma bomba
-              | otherwise       = bombaVisivel xss          -- aplica recursao pro restante da lista novamente
+              | temBomba xs     = True                          -- Se o estado do campo for Bomba, aparece uma bomba
+              | otherwise       = bombaVisivel xss              -- aplica recursao pro restante da lista novamente
               where temBomba xs = not $ all naoTemBomba xs
-                    naoTemBomba Bomba   = False             -- Se o estado do campo for Bomba, retora "falso" para "nao tem bomba", o que retorna "verdedeiro" para "tem bomba"
-                    naoTemBomba _       = True              -- em outro caso, nao tem bomba mesmo.
+                    naoTemBomba Bomba   = False                 -- Se o estado do campo for Bomba, retora "falso" para "nao tem bomba", o que retorna "verdedeiro" para "tem bomba"
+                    naoTemBomba _       = True                  -- em outro caso, nao tem bomba mesmo.
 
 
 analisaEntrada :: String -> Coordenada
@@ -88,16 +88,16 @@ gerarJogo w h n g = [zipWith combinar ms cs | (ms, cs) <- zip bombaMap mapaPeso]
 
 
 gerarCoordenadas :: RandomGen g => Int -> Int -> Int -> g -> [Coordenada]
-gerarCoordenadas w h n g = zip xs ys                        -- concatena os x's e y's na forma de coordenada
+gerarCoordenadas w h n g = zip xs ys                            -- concatena os x's e y's na forma de coordenada
     where
-      xs = take n (randomRs (0, w-1) g)                     -- pega 40 x's aleatorios entre 0 e tamanho do eixo X
-      ys = drop n $ take (n*2) $ randomRs (0, h-1) g        -- pega 2 vezes o tamanho (40) e dropa metade
+      xs = take n (randomRs (0, w-1) g)                         -- pega 40 x's aleatorios entre 0 e tamanho do eixo X
+      ys = drop n $ take (n*2) $ randomRs (0, h-1) g            -- pega 2 vezes o tamanho (40) e dropa metade
 
 
 gerarMinas :: Int -> Int -> Int -> [Coordenada] -> Minas
-gerarMinas w h n bombas = foldr localBomba mina bombas   -- aplica "localbomba" colocando o estado da matriz "mina" onde for bomba efetivamente
+gerarMinas w h n bombas = foldr localBomba mina bombas          -- aplica "localbomba" colocando o estado da matriz "mina" onde for bomba efetivamente
     where
-      mina              = fazMatriz w h (Peso 0)          -- cria matriz com esses parametros
+      mina              = fazMatriz w h (Peso 0)                -- cria matriz com esses parametros
       localBomba p mina = trocarIndiceMatriz p mina Bomba
 
 
@@ -108,20 +108,19 @@ geraMapaPeso w h bombas = foldr succCoordenada mapaPeso emTornoCoordenadas  -- a
       mapaPeso                         = replicate w $ replicate h 0        -- replica "w" vezes a replica de "h" vezes 0 (pesos iniciais)
       succCoordenada p@(x, y) mapaPeso = trocarIndiceMatriz p mapaPeso      -- funcao "succCoordenada"
                                    $ succ (mapaPeso !! x !! y)
-                                     
 
 
-emTorno :: Int -> Int -> Coordenada -> [Coordenada]               -- mapeia coordenadas em torno
+emTorno :: Int -> Int -> Coordenada -> [Coordenada]             -- mapeia coordenadas em torno
 emTorno w h (x, y) =
     filter (noLimite w h) [(x-1, y+1), (x, y+1), (x+1, y+1),
                            (x-1, y),             (x+1, y),
                            (x-1, y-1), (x, y-1), (x+1, y-1)]
 
 
-mostrarMinas ::  Minas -> IO ()                                       -- funcao de imprimir o campo
+mostrarMinas ::  Minas -> IO ()                                 -- funcao de imprimir o campo
 mostrarMinas w = putStrLn $ mostrarMatrizCom mostrarPonto w
 
-mostrarPonto :: Estado Int -> String                                  -- funcao que imprime a situacao atual da coordenada
+mostrarPonto :: Estado Int -> String                            -- funcao que imprime a situacao atual da coordenada
 mostrarPonto Bomba      = mostrarCentralizado espaco "*"
 mostrarPonto Fechado    = mostrarCentralizado espaco "#"
 mostrarPonto (Peso 0)   = mostrarCentralizado espaco " "
@@ -133,21 +132,21 @@ mostrarCentralizado l x = (replicate espacoEsq ' ') ++ x ++ (replicate espacoDir
           espacoDir  =  l - espacoEsq - (length x)                                      -- Espaço a direira é o espaço menos o espaço a esquerda menos o tamanho do conteudo da celula
 
 
-mapaMatriz :: (a -> b) -> [[a]] -> [[b]]      -- Mapeia uma função sobre uma lista de listas
+mapaMatriz :: (a -> b) -> [[a]] -> [[b]]                        -- Mapeia uma função sobre uma lista de listas
 mapaMatriz f xss = map (map f) xss
 
 
-mostrarMatrizCom :: (a -> String) -> [[a]] -> String    -- Cria uma string a partir de uma matriz de strings, insere novos caracteres de linha entre as strings originais
+mostrarMatrizCom :: (a -> String) -> [[a]] -> String            -- Cria uma string a partir de uma matriz de strings, insere novos caracteres de linha entre as strings originais
 mostrarMatrizCom f = unlines . adicionaBorda . map concat . mapaMatriz f . transpose 
 
 
 adicionaBorda :: [String] -> [String]
-adicionaBorda xs = [numHorizontal]              -- Essa função recebe a String xs e retorna uma sString maior composta pelos números da borda superior,
-               ++ [bordaHorizontal l]           -- linha da borda superior, linha da borda lateral e a matriz, seja esta #, ou os pesos (1,2,3), ou ainda
-               ++ map bordaVertical xs          -- a bomba e termina preenchedo a linha da outra lateral e a inferior
+adicionaBorda xs = [numHorizontal]                              -- Essa função recebe a String xs e retorna uma sString maior composta pelos números da borda superior,
+               ++ [bordaHorizontal l]                           -- linha da borda superior, linha da borda lateral e a matriz, seja esta #, ou os pesos (1,2,3), ou ainda
+               ++ map bordaVertical xs                          -- a bomba e termina preenchedo a linha da outra lateral e a inferior
                ++ [(bordaHorizontal l)]
-    where l                  = length (xs !! 0) -- A largura é do tamanho do comprimento da posição 0 de xs 
-          a                  = length xs        -- A altura é do tamanho do comprimento de xs
+    where l                  = length (xs !! 0)                 -- A largura é do tamanho do comprimento da posição 0 de xs 
+          a                  = length xs                        -- A altura é do tamanho do comprimento de xs
           numHorizontal      = "  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19"
           bordaHorizontal l  = " " ++ (replicate l '-') ++ " "
           bordaVertical xs   = "|" ++ xs ++ "|"
@@ -162,11 +161,11 @@ substituirIndice indice xs x = take indice xs ++ ( x : (drop (indice+1) xs))
 
 
 
-fazMatriz :: Int -> Int -> a -> [[a]]           -- Cria um preenchimento de matriz com um determinado elemento
-fazMatriz l a e = replicate l $ replicate a e   -- Cria uma lista de comprimento fornecida pelo primeiro argumento e os itens com valor do segundo argumento
+fazMatriz :: Int -> Int -> a -> [[a]]                          -- Cria um preenchimento de matriz com um determinado elemento
+fazMatriz l a e = replicate l $ replicate a e                  -- Cria uma lista de comprimento fornecida pelo primeiro argumento e os itens com valor do segundo argumento
 
 
-noLimite :: Int -> Int -> Coordenada -> Bool -- Verifica se a coordenada digitada está dentro do campo
+noLimite :: Int -> Int -> Coordenada -> Bool                   -- Verifica se a coordenada digitada está dentro do campo
 noLimite l a (x, y)
     | x < 0     = False
     | x >= l    = False
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Campo Minado em Haskell
 Trabalho da disciplina Linguagem de Programação - Engenharia de Computação - Universidade de Brasília
 
-##Equipe
+## Equipe
 * Anna Thais Costa Lopes
 * Aécio Fernandes Galiza Magalhães
